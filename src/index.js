fd5a31c0cc7c63bf272211c8d855c850d3249734
TroyLeung <troylchwork@gmail.com>
2025-11-27 02:26:15 +0800

    [FIX] deploy

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ root.render(
         </I18nextProvider>
     </BrowserRouter>
 );
-
+// force deploy
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
